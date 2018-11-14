612ce6613c7e8c0fdcaabafceda860e7975a84ff
Andre Artus <andre.artus@gmail.com>
2018-11-14 21:08:39 +0200

    Clear DB on start

--- kotlin-spring-demo/src/main/kotlin/com/example/kotlinspringdemo/KotlinSpringDemoApplication.kt
+++ kotlin-spring-demo/src/main/kotlin/com/example/kotlinspringdemo/KotlinSpringDemoApplication.kt
@@ -1,10 +1,19 @@
 package com.example.kotlinspringdemo
 
+import com.example.kotlinspringdemo.data.ProjectRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class KotlinSpringDemoApplication
+class KotlinSpringDemoApplication {
+    @Bean
+    fun initiatingProcess(repo: ProjectRepository) = CommandLineRunner {
+        println("========== Clear all entities ==========")
+        repo.deleteAll()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<KotlinSpringDemoApplication>(*args)
